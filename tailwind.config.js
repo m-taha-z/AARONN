251d1f1d4195fbb233b571f1cc99b1e338d7d6a8
taha mahdy <you@example.com>
2024-01-13 19:18:56 +0330

    v-3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,37 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
-      "syne":['Syne, sans-serif'],
-      "rubik":['Rubik, sans-serif'],
+      rubik: ["Rubik, sans-serif"],
+      syne: ["Syne, sans-serif"],
     },
-
     extend: {
       colors: {
-        "primary":"#FF9143",
-        "secondry":{
-          100:'#1A1A1A',
-          200:'#262A2C',
-          300:'#696969',
-          400:'#FFFFFF',
-        }
-      }
+        "Brand-Colors": "#FF9143",
+        SC: {
+          1: "#1A1A1A",
+          2: "#262A2C",
+          3: "#696969",
+          4: "#FFFFFF",
+          5:"#ffffff84",
+          6:"#A8A8A8",
+          7:"#606060",
+          border:"#FF9142",
+        },
+        Greyscale: {
+          1: "#6C727F",
+          2: "#4D5461",
+          3: "#E5E6EB",
+          4: "#D3D5DA",
+          5: "#9EA3AE",
+          6: "#6C727F",
+          7: "#4D5461",
+          8: "#394150",
+          9: "#212936",
+          10: "#161D21",
+        },
+      },
     },
+
+    plugins: [],
   },
-  plugins: [],
-}
+};
